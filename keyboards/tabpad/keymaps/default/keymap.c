a6f5ba05ae5c6514785af622d52ae6bd6407c7ec
Pedro-748 <paytonhouts00@gmail.com>
2024-07-18 14:08:36 -0500

    QMK Try1

--- keyboards/tabpad/keymaps/default/keymap.c
+++ keyboards/tabpad/keymaps/default/keymap.c
@@ -1,24 +1,13 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
-    [0] = LAYOUT(
+
+    [0] = LAYOUT_tabpad_5x4(
+        KC_NUM_LOCK,   KC_TAB,   KC_BACKSPACE,
         KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        KC_P1,   KC_P2,   KC_P3,
         KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
     )
 };
